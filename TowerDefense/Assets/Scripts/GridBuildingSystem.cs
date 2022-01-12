8f792f30d037d782d23e0289d0c2368f9763fb65
Juan Carlos Sánchez Guzmán <jcarlos0305@hotmail.com>
2022-01-12 15:54:09 -0600

    Selectable cells grid implementation

---
+++ TowerDefense/Assets/Scripts/GridBuildingSystem.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Defense.Utils;
+
+public class GridBuildingSystem : MonoBehaviour {
+    public static GridBuildingSystem Instance { get; private set; }
+
+    public event EventHandler OnSelectedChanged;
+
+    public event EventHandler OnObjectPlaced;
+
+    private GridXZ<GridObject> grid;
+
+    [SerializeField] private List<PlacedObjectTypeSO> placedObjectTypeSOList = null;
+    private PlacedObjectTypeSO placedObjectTypeSO;
+
+    private PlacedObjectTypeSO.Dir dir = PlacedObjectTypeSO.Dir.Down;
+
+    private void Awake() {
+        Instance = this;
+
+        int gridWidth = 10;
+        int gridHeight = 10;
+        float cellSize = 30f;
+        placedObjectTypeSO = placedObjectTypeSOList[0];
+        grid = new GridXZ<GridObject>(gridWidth, gridHeight, cellSize, new Vector3(0, 0, 0), (GridXZ<GridObject> g, int x, int y, bool isEmpty) => new GridObject(g, x, y, isEmpty));
+    }
+
+    public class GridObject {
+        private GridXZ<GridObject> grid;
+        private int x;
+        private int y;
+        private bool canBuild;
+
+        public PlacedObject placedObject;
+
+        public GridObject(GridXZ<GridObject> grid, int x, int y, bool isEmpty) {
+            this.grid = grid;
+            this.x = x;
+            this.y = y;
+            this.canBuild = isEmpty;
+            placedObject = null;
+        }
+
+        public void SetPlacedObject(PlacedObject placedObject) {
+            this.placedObject = placedObject;
+            grid.TriggerGridObjectChanged(x, y);
+        }
+
+        public void ClearPlacedObject() {
+            placedObject = null;
+            grid.TriggerGridObjectChanged(x, y);
+        }
+
+        public bool CanBuild() {
+            return canBuild && placedObject == null;
+        }
+
+        public override string ToString() {
+            return x + ", " + y + "\n" + placedObject;
+        }
+
+        public PlacedObject GetPlacedObject() {
+            return placedObject;
+        }
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 mousePosition = Mouse3D.GetMouseWorldPosition();
+            grid.GetXZ(mousePosition, out int x, out int z);
+
+            Vector2Int placedObjectOrigin = new Vector2Int(x, z);
+            placedObjectOrigin = grid.ValidateGridPosition(placedObjectOrigin);
+
+            // Test Can Build
+            List<Vector2Int> gridPositionList = placedObjectTypeSO.GetGridPositionList(placedObjectOrigin, dir);
+
+            bool canBuild = true;
+            foreach (Vector2Int gridPosition in gridPositionList) {
+                if (!grid.GetGridObject(gridPosition.x, gridPosition.y).CanBuild()) {
+                    canBuild = false;
+                    break;
+                }
+            }
+
+            if (canBuild) {
+                Vector2Int rotationOffset = placedObjectTypeSO.GetRotationOffset(dir);
+                Vector3 placedObjectWorldPosition = grid.GetWorldPosition(x, z) + new Vector3(rotationOffset.x, 0, rotationOffset.y) * grid.GetCellSize();
+
+                PlacedObject placedObject = PlacedObject.Create(placedObjectWorldPosition, placedObjectOrigin, dir, placedObjectTypeSO);
+
+                foreach (Vector2Int gridPosition in gridPositionList) {
+                    grid.GetGridObject(gridPosition.x, gridPosition.y).SetPlacedObject(placedObject);
+                }
+
+                OnObjectPlaced?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R)) {
+            dir = PlacedObjectTypeSO.GetNextDir(dir);
+        }
+
+        if (Input.GetMouseButtonDown(1)) {
+            Vector3 mousePosition = Mouse3D.GetMouseWorldPosition();
+            grid.GetXZ(mousePosition, out int x, out int z);
+
+            GridObject gridObject = grid.GetGridObject(x, z);
+
+            // Valid Grid Position
+            PlacedObject placedObject = gridObject.GetPlacedObject();
+
+            if (placedObject != null) {
+                // Demolish
+                placedObject.DestroySelf();
+
+                List<Vector2Int> gridPositionList = placedObject.GetGridPositionList();
+                foreach (Vector2Int gridPosition in gridPositionList) {
+                    grid.GetGridObject(gridPosition.x, gridPosition.y).ClearPlacedObject();
+                }
+            }
+        }
+    }
+
+    private void DeselectObjectType() {
+        placedObjectTypeSO = null; RefreshSelectedObjectType();
+    }
+
+    private void RefreshSelectedObjectType() {
+        OnSelectedChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public Vector2Int GetGridPosition(Vector3 worldPosition) {
+        grid.GetXZ(worldPosition, out int x, out int z);
+        return new Vector2Int(x, z);
+    }
+
+    public Vector3 GetMouseWorldSnappedPosition() {
+        Vector3 mousePosition = Mouse3D.GetMouseWorldPosition();
+        grid.GetXZ(mousePosition, out int x, out int z);
+
+        if (placedObjectTypeSO != null) {
+            Vector2Int rotationOffset = placedObjectTypeSO.GetRotationOffset(dir);
+            Vector3 placedObjectWorldPosition = grid.GetWorldPosition(x, z) + new Vector3(rotationOffset.x, 0, rotationOffset.y) * grid.GetCellSize();
+            return placedObjectWorldPosition;
+        } else {
+            return mousePosition;
+        }
+    }
+
+    public Quaternion GetPlacedObjectRotation() {
+        if (placedObjectTypeSO != null) {
+            return Quaternion.Euler(0, placedObjectTypeSO.GetRotationAngle(dir), 0);
+        } else {
+            return Quaternion.identity;
+        }
+    }
+
+    public PlacedObjectTypeSO GetPlacedObjectTypeSO() {
+        return placedObjectTypeSO;
+    }
+}
